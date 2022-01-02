9ab3d2be1dda7c0274d67ae2da546972dc5d3649
bernstein <bernsteining@protonmail.com>
2022-01-02 22:42:52 +0100

    Change keypad layout

--- src/input.rs
+++ src/input.rs
@@ -22,7 +22,7 @@ pub fn set_keypad(k: &Rc<RefCell<[bool; 16]>>) {
     append_to_body(&keypad);
 
     for (index, key) in [
-        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",
+        "1", "2", "3", "C", "4", "5", "6", "D", "7", "8", "9", "E", "A", "0", "B", "F",
     ]
     .iter()
     .enumerate()
