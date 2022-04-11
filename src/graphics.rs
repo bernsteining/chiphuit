70da484cbfbfb20d5cfef22dd74ec68dc6d10e66
bernstein <bernsteining@protonmail.com>
2022-04-11 11:13:09 +0200

    Refactor screen refresh

--- src/graphics.rs
+++ src/graphics.rs
@@ -59,22 +59,15 @@ pub fn set_canvas() -> web_sys::CanvasRenderingContext2d {
 /// draw_screen(context, boolean_screen);
 /// ```
 pub fn draw_screen(context: &CanvasRenderingContext2d, boolean_screen: [bool; 64 * 32]) {
-    let mut graphic_screen = [0; 64 * 32 * 4];
-    for (i, x) in boolean_screen.iter().enumerate() {
-        if x == &false {
-            graphic_screen[4 * i + 1] = 0;
-            graphic_screen[4 * i + 2] = 0;
-            graphic_screen[4 * i + 3] = 0;
-            graphic_screen[4 * i] = 0;
-        } else {
-            graphic_screen[4 * i + 1] = 255;
-            graphic_screen[4 * i + 2] = 255;
-            graphic_screen[4 * i + 3] = 255;
-            graphic_screen[4 * i] = 255;
-        }
-    }
+    let screen: Vec<u8> = boolean_screen
+        .iter()
+        .flat_map(|x| match x {
+            false => [0u8; 4],
+            true => [255u8; 4],
+        })
+        .collect();
+
     let graphic_screen =
-        ImageData::new_with_u8_clamped_array_and_sh(Clamped(&graphic_screen), WIDTH, HEIGHT)
-            .unwrap();
+        ImageData::new_with_u8_clamped_array_and_sh(Clamped(&screen), WIDTH, HEIGHT).unwrap();
     context.put_image_data(&graphic_screen, 0.0, 0.0).unwrap();
 }
